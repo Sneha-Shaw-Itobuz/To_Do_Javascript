3443fdbe70fc694e4276787f51f44167e768a9f2
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-02-07 11:53:09 +0530

    `changes`

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let taskInput = document.getElementById("taskInput");
 let list = document.getElementById("list");
 let clear = document.getElementById("clear");
 let all = document.getElementById("All");
-let incomplete = document.getElementById("incomplete")
-let completed = document.getElementById("completed")
-let filters = document.querySelectorAll('.filters button')
+let incomplete = document.getElementById("incomplete");
+let completed = document.getElementById("completed");
+let filters = document.querySelectorAll(".filters button");
 
 function add() {
   if (taskInput.value.length == 0) {
@@ -24,7 +24,7 @@ function add() {
                 </button>
             </div>
         `;
-filters[0].classList.add('active')
+    filters[0].classList.add("active");
 
     taskInput.value = "";
     let taskDelete = document.querySelectorAll(".delete");
@@ -42,61 +42,53 @@ filters[0].classList.add('active')
   }
 }
 
-taskInput.addEventListener('keypress',(e)=>{
-    if(e.key==='Enter'){
-        add()
-    }
-} )
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    add();
+  }
+});
 
 clear.onclick = function () {
-    let taskComplete = document.querySelectorAll(".complete");
-    for (let i = 0; i < taskComplete.length; i++) {
-        
-        if(taskComplete[i].parentNode.style.textDecoration){
-            taskComplete[i].parentNode.remove()
-        }
-      }
+  let taskComplete = document.querySelectorAll(".complete");
+  for (let i = 0; i < taskComplete.length; i++) {
+    if (taskComplete[i].parentNode.style.textDecoration) {
+      taskComplete[i].parentNode.remove();
+    }
+  }
 };
 
-completed.onclick= function (){
-    let taskComplete = document.querySelectorAll(".complete");
-    for (let i = 0; i < taskComplete.length; i++) {
-        
-        if(!taskComplete[i].parentNode.style.textDecoration){
-            taskComplete[i].parentNode.style.display="none"
-        }
-        else{
-            taskComplete[i].parentNode.style.display="flex"
-        }
-      }
-      filters[2].classList.add('active')
-      filters[1].classList.remove('active')
-      filters[0].classList.remove('active')
-
-
-}
-incomplete.onclick= function (){
-    let taskComplete = document.querySelectorAll(".complete");
-    for (let i = 0; i < taskComplete.length; i++) {
-        
-        if(taskComplete[i].parentNode.style.textDecoration){
-            taskComplete[i].parentNode.style.display="none"
-        }
-        else{
-            taskComplete[i].parentNode.style.display="flex"
-        }
-      }
-      filters[1].classList.add('active')
-filters[0].classList.remove('active')
-filters[2].classList.remove('active')
-}
-all.onclick= function (){
-    let taskComplete = document.querySelectorAll(".complete");
-    for (let i = 0; i < taskComplete.length; i++) {
-            taskComplete[i].parentNode.style.display="flex"
-      }
-      filters[0].classList.add('active')
-      filters[1].classList.remove('active')
-      filters[2].classList.remove('active')
-}
-
+completed.onclick = function () {
+  let taskComplete = document.querySelectorAll(".complete");
+  for (let i = 0; i < taskComplete.length; i++) {
+    if (!taskComplete[i].parentNode.style.textDecoration) {
+      taskComplete[i].parentNode.style.display = "none";
+    } else {
+      taskComplete[i].parentNode.style.display = "flex";
+    }
+  }
+  filters[2].classList.add("active");
+  filters[1].classList.remove("active");
+  filters[0].classList.remove("active");
+};
+incomplete.onclick = function () {
+  let taskComplete = document.querySelectorAll(".complete");
+  for (let i = 0; i < taskComplete.length; i++) {
+    if (taskComplete[i].parentNode.style.textDecoration) {
+      taskComplete[i].parentNode.style.display = "none";
+    } else {
+      taskComplete[i].parentNode.style.display = "flex";
+    }
+  }
+  filters[1].classList.add("active");
+  filters[0].classList.remove("active");
+  filters[2].classList.remove("active");
+};
+all.onclick = function () {
+  let taskComplete = document.querySelectorAll(".complete");
+  for (let i = 0; i < taskComplete.length; i++) {
+    taskComplete[i].parentNode.style.display = "flex";
+  }
+  filters[0].classList.add("active");
+  filters[1].classList.remove("active");
+  filters[2].classList.remove("active");
+};
